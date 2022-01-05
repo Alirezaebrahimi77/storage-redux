de1c5f35366f07f6ec1ef7d2767bdc102cc46dba
Alireza <alireza27163@gmail.com>
2022-01-05 15:42:12 +0200

    fixed ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ const Header = () => {
     return (
         <header className='w-full bg-white shadow-sm flex flex-row justify-between align-middle py-6 px-4'>
             <div>
-                <h2 className='cursor-pointer'>Storage Tracker</h2>
+                <h2 className='cursor-pointer'>Storage App</h2>
             </div>
-            <nav>
+            <nav className='text-sm'>
                 <ul className='flex gap-x-2 cursor-pointer'>
-                    <li>Products</li>
+                    <li>Manage Categories</li>
                 </ul>
 
             </nav>
--- src/components/StorageList.js
+++ src/components/StorageList.js
@@ -42,8 +42,8 @@ const StorageList = () => {
     }
     return (
         <div className='w-full p-12 h-full'>
-            <div className='w-90 xl:w-90 m-auto border-2 border-gray-100 p-3 shadow-sm rounded-sm'>
-                <p className='text-xl text-center mb-4'>Add Product:</p>
+            <div className='w-90 md:w-[700px] m-auto border-2 border-gray-100 p-3 shadow-sm rounded-sm'>
+                <p className='text-xl text-center mb-6'>Add Product:</p>
                 <form onSubmit={submitHandler}>
                     <div className='flex items-center mb-5'>
                         <label htmlFor="productName" className='text-sm mr-4'>Name:</label>
@@ -53,7 +53,7 @@ const StorageList = () => {
                     </div>
                     <div className='flex items-center'>
                         <label htmlFor="productCategory" className='text-sm mr-4'>Category:</label>
-                        <select name="category" id="productCategory" className='border-2 border-emerald-400 outline-none py-1 px-6 rounded-md text-sm bg-white' value={product.category} onChange={changeHandler} >
+                        <select name="category" id="productCategory" className='border-2 border-gray-100 outline-none py-1 px-6 rounded-md text-sm bg-white' value={product.category} onChange={changeHandler} >
                             <option value="select" disabled>Select</option>
                             {/* <option value="electric">Electric</option>
                             <option value="wood">Wood</option>
@@ -69,7 +69,7 @@ const StorageList = () => {
                 </form>
 
             
-               <p className='text-emerald-900 text-sm underline mt-6 cursor-pointer hover:no-underline' onClick={() => SetShowCategory(!showCategory)}>Add category</p>
+               <p className='text-emerald-900 text-sm underline mt-6 cursor-pointer hover:no-underline' onClick={() => SetShowCategory(!showCategory)}>No Category? Add one</p>
 
 
                 
@@ -78,7 +78,10 @@ const StorageList = () => {
             
 
             </div>
-            <div className={`w-full border-2 border-gray-100 rounded-sm fixed bottom-0 right-0 left-0 transition duration-300 translate-y-full ${showCategory && "translate-y-0"}`}>
+            <div className={`w-full border-2 border-gray-100 rounded-sm fixed bottom-0 right-0 left-0 transition duration-300 translate-y-full ${showCategory && "translate-y-0"} flex justify-center bg-white`}>
+                <div className='w-full md:w-[700px]'>
+
+                
                     <div className="w-full flex justify-end py-2 px-4">
                         <AiOutlineCloseCircle className='text-emerald-400 text-xl cursor-pointer' onClick={() => SetShowCategory(!showCategory)}/>
                         
@@ -89,6 +92,7 @@ const StorageList = () => {
                             <input type="text" placeholder='Type your new catrgory' value={category.category} onChange={(e) => setCategory(e.target.value)} className='flex-1 border-2 rounded-md text-sm outline-emerald-400 py-1 px-3 mr-6'/>
                             <button className='py-1 px-4 rounded-md text-white text-sm bg-emerald-400'>Add category</button>
                         </form>
+                </div>
             </div>
             
             
